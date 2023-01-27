41f69d90179367af28dc44c121b93de3b6be96cb
chanceoneill <chanceoneill56@gmail.com>
2023-01-26 19:26:43 -0500

    test

---
+++ Mywork/Examples.java
@@ -0,0 +1,14 @@
+package Mywork;
+public class Examples {
+   public static void main(String[] args) {
+        System.out.println( "this class rocks!");
+
+        // void type has no returned value
+        // void type is used for methods that do not return a value
+
+        // what are java types?
+        // int, double, boolean, String, char, void
+        String dog = "Jim";
+        System.out.println(dog);
+    }
+}
---
+++ Mywork/GettingStarted.java
@@ -0,0 +1,7 @@
+package Mywork;
+public class GettingStarted {
+    public static void main(String[] args) {
+        System.out.println( "Hello world");
+
+    }
+}
